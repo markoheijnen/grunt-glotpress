cd4e793697381786c615d8d164d0b4998ca00fa6
Marko Heijnen <info@markoheijnen.com>
2014-07-29 19:47:45 +0200

    Change task to glotpress_download.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 		},
 
 		// Configuration to be run (and then tested).
-		glotpress: {
+		glotpress_download: {
 			default_options: {
 				options: {
 				},
@@ -65,7 +65,7 @@ module.exports = function(grunt) {
 
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
 	// plugin's task(s), then test the result.
-	grunt.registerTask('test', ['clean', 'glotpress', 'nodeunit']);
+	grunt.registerTask('test', ['clean', 'glotpress_download', 'nodeunit']);
 
 	// By default, lint and run all tests.
 	grunt.registerTask('default', ['jshint', 'test']);
--- tasks/download.js
+++ tasks/download.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('glotpress', 'Gets translations from a GlotPress installation', function() {
+  grunt.registerMultiTask('glotpress_download', 'Gets translations from a GlotPress installation', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
