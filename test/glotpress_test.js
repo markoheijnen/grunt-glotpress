1c31cf84229234ba426a0d4bbd1c0569b752388a
Marko Heijnen <info@markoheijnen.com>
2015-02-13 19:47:23 +0100

    First test for strip_trailing_slash. See #17.

--- test/glotpress_test.js
+++ test/glotpress_test.js
@@ -1,45 +1,29 @@
 'use strict';
 
 var 
-	grunt = require('grunt'),
-	path = require('path'),
-	exec = require('child_process').exec,
-	execOptions = {
-		cwd: path.join(__dirname, '..')
-	}
+	glotpress_downloader = require('../lib/downloader');
 ;
 
-/*
-	======== A Handy Little Nodeunit Reference ========
-	https://github.com/caolan/nodeunit
-
-	Test methods:
-		test.expect(numAssertions)
-		test.done()
-	Test assertions:
-		test.ok(value, [message])
-		test.equal(actual, expected, [message])
-		test.notEqual(actual, expected, [message])
-		test.deepEqual(actual, expected, [message])
-		test.notDeepEqual(actual, expected, [message])
-		test.strictEqual(actual, expected, [message])
-		test.notStrictEqual(actual, expected, [message])
-		test.throws(block, [error], [message])
-		test.doesNotThrow(block, [error], [message])
-		test.ifError(value)
-*/
-
-exports.glotpress_download = {
-	setUp: function(done) {
+exports.test_glotpress_download = {
+	setUp: function(callback) {
 		// setup here if necessary
-		done();
+		callback();
 	},
-	empty_options: function(test) {
-		test.expect(1);
+	tearDown: function (callback) {
+		// clean up
+		callback();
+	},
 
-		exec('grunt glotpress_download:empty_options', execOptions, function(error, stdout) {
-			test.equal(-1, -1, 'should not return anything');
-			test.done();
-		});
+	strip_trailing_slash_without_slash: function(test) {
+		test.equal( glotpress_downloader.strip_trailing_slash( 'http://google.nl'), 'http://google.nl', 'Should return url without trailing slash');
+
+		test.done();
+	},
+
+	strip_trailing_slash_with_slash: function(test) {
+		test.equal( glotpress_downloader.strip_trailing_slash( 'http://google.nl/'), 'http://google.nl', 'Should return url without trailing slash');
+
+		test.done();
 	}
+
 };
