b9f72e82e0573ab92b89e8f10a3fc3d9791120c0
Marko Heijnen <info@markoheijnen.com>
2015-02-15 23:42:09 +0100

    Move options merging in lib and build up an own callback. See #16 and #17

--- tasks/download.js
+++ tasks/download.js
@@ -14,41 +14,18 @@ module.exports = function(grunt) {
 	var is_done;
 
 	grunt.registerMultiTask('glotpress_download', 'Gets translations from a GlotPress installation', function() {
-		is_done = downloader.is_done = this.async();
+		is_done = this.async();
 
 		// Merge task-specific and/or target-specific options with these defaults.
-		var options = this.options({
-			domainPath: 'languages',
-			url: false,
-			slug: false,
-			textdomain: false,
-			file_format: '%domainPath%/%textdomain%-%wp_locale%.%format%',
-			formats: [
-				'po',
-				'mo'
-			],
-			filter: {
-				translation_sets: false,
-				minimum_percentage: 30,
-				waiting_strings: false
+		var options = this.options();
+
+		downloader.download_translations( options, function(success, message) {
+			if ( message && ! success ) {
+				grunt.fail.report("All required options aren't filled in.");
 			}
-		});
 
-		if ( ! options.url || ! options.slug ) {
-			grunt.fail.report("All required options aren't filled in.");
-			is_done(false);
-		}
-
-		options.url        = downloader.strip_trailing_slash( options.url );
-		options.domainPath = downloader.strip_trailing_slash( options.domainPath );
-
-		if ( ! options.textdomain ) {
-			options.textdomain = options.slug;
-		}
-
-		var api_url = options.url + '/api/projects/' + options.slug;
-
-		downloader.get_project_data( api_url, options );
+			is_done(success);
+		} );
 	});
 
 };
--- test/glotpress_test.js
+++ test/glotpress_test.js
@@ -14,6 +14,25 @@ exports.test_glotpress_download = {
 		callback();
 	},
 
+
+	merge_defaults_test_default: function(test) {
+		var defaults = { test: '1' }
+		var options  = glotpress_downloader.merge_defaults( {}, defaults );
+
+		test.equal( options.test, '1' );
+
+		test.done();
+	},
+
+	merge_defaults_test_overwrite: function(test) {
+		var defaults = { test: '1' }
+		var options  = glotpress_downloader.merge_defaults( { test: '2' }, defaults );
+
+		test.equal( options.test, '2' );
+
+		test.done();
+	},
+
 	strip_trailing_slash_without_slash: function(test) {
 		test.equal( glotpress_downloader.strip_trailing_slash( 'http://google.nl'), 'http://google.nl', 'Should return url without trailing slash');
 
