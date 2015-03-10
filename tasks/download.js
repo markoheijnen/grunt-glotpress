34d75c72a565599828fa8f0b6b9ce737efe01dd6
Marko Heijnen <info@markoheijnen.com>
2015-03-10 14:22:53 +0100

    Add cwd support

--- tasks/download.js
+++ tasks/download.js
@@ -10,14 +10,21 @@
 
 
 module.exports = function(grunt) {
-	var downloader = require( '../lib/downloader' );
+	var downloader = require( '../lib/downloader' ),
+		path = require( 'path' );
+
 	var is_done;
 
 	grunt.registerMultiTask('glotpress_download', 'Gets translations from a GlotPress installation', function() {
 		is_done = this.async();
 
 		// Merge task-specific and/or target-specific options with these defaults.
-		var options = this.options();
+		var options = this.options({
+			cwd: process.cwd()
+		});
+
+		options.cwd = path.resolve( process.cwd(), options.cwd );
+		grunt.file.setBase( options.cwd );
 
 		downloader.download_translations( options, function(success, message) {
 			if ( message && ! success ) {
